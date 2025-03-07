c3f1644520f36986fbac5ff10274648b191ebebd
Daisycutie <144363466+Daisycutie@users.noreply.github.com>
2025-03-08 04:11:30 +0700

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
-
+from page_1 import page_page1 
+from page_2 import page_page2 
+from page_3 import page_page3
+from page_4 import page_page4 
 st.sidebar.title("เลือกหน้า")
 page = st.sidebar.radio("Select page", ("Machine Learning", "Neural Network", 
                                         "Demo Neural Network", "Demo Machine Learning"))   
